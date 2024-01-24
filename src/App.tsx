53c2c29195fd97f82f27967cc1f506c9d9cf686b
takashi823 <uchimura.takashi724@gmail.com>
2024-01-24 11:28:31 +0900

    fix: location追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@
 import { Box, SxProps, Theme } from "@mui/material";
 
 // 使用するコンポートをインポート
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation  } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { SamplePage } from "./pages/SamplePage";
 
 // Appのメインの実装（表示する画面とpathを定義）
 export const App = () => {
+  const URL = process.env.PUBLIC_URL;
+  const location = useLocation();
+
   return (
     <Box className="App" sx={sx}>
-      <Routes>
+      <Routes location={location} key={location.pathname}>
         {/* トップページ*/}
-        <Route path="/" element={<HomePage />} />
+        <Route path={URL + "/"} element={<HomePage />} />
         {/* サンプルページ */}
-        <Route path="/sample" element={<SamplePage />} />
+        <Route path={URL + "/sample"} element={<SamplePage />} />
       </Routes>
     </Box>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,8 @@ import { BrowserRouter } from "react-router-dom";
 import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
